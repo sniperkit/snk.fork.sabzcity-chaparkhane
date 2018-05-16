75b0acb772e20afdcbba3ccde618c3797efd8721
Omid Hekayati <omid@sabz.city>
2018-05-16 13:41:29 +0430

    Some Improvments in Idea

--- example/logic/validate-national-code.go
+++ example/logic/validate-national-code.go
@@ -2,12 +2,12 @@
 
 package logic
 
-// ReqNationalCodeValidation : The standard request structure of "NationalCodeValidation()".
+// ReqNationalCodeValidation : The request structure of "NationalCodeValidation()".
 type ReqNationalCodeValidation struct {
 	NationalCode uint `valid:"required" restful:"url" jsonrpc:"body" grpc:"body" protobuf:"1" json:"NationalCode" xml:"NationalCode"`
 }
 
-// ResNationalCodeValidation : The standard response structure of "NationalCodeValidation()".
+// ResNationalCodeValidation : The response structure of "NationalCodeValidation()".
 type ResNationalCodeValidation struct {
 	Valid bool `restful:"body" jsonrpc:"body" grpc:"body" protobuf:"1" json:"Valid" xml:"Valid"` // UUID of user
 }
@@ -15,8 +15,10 @@ type ResNationalCodeValidation struct {
 // NationalCodeValidation : Check National code and say it is true or false.
 // restful-URI : "/{ServiceName}/{ServiceVersion}/{MethodName}/"
 // restful-Method : "POST"
+// restful-Headers-Access-Control-Allow-Origin : "https://sabz.city"
 // jsonrpc-URI : "/"
 // jsonrpc-Method : "POST"
+// jsonrpc-Headers-Access-Control-Allow-Origin : "*"
 // ...
 func NationalCodeValidation(logicRequest *ReqNationalCodeValidation) (*ResNationalCodeValidation, error) {
 	var logicResponse ResNationalCodeValidation
